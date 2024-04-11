651b6e56b72cf32fe7134540d8c8256745e4a4da
Nishanth Ramasamy <59798118+nishanthramasamy@users.noreply.github.com>
2024-04-11 18:52:29 +0530

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,74 @@
+import string
+import spacy
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import pandas as pd
+import streamlit as st
+
+ylp = pd.read_csv(rf"F:\guvi_test\sentiment_Analysis\yelp_labelled.txt", sep='\t', names=['reviews', 'label'] )
+print(ylp.head())
+print(ylp.isnull().sum())
+print(ylp.shape)
+
+amz = pd.read_csv(rf"F:\guvi_test\sentiment_Analysis\amazon_cells_labelled.txt", sep='\t', names=['reviews', 'label'])
+print(amz.isnull().sum())
+print(amz.head())
+print(amz.shape)
+
+imdb = pd.read_csv(rf"F:\guvi_test\sentiment_Analysis\imdb_labelled.txt", sep='\t', names=['reviews', 'label'])
+print(imdb.isnull().sum())
+print(imdb.head())
+print(imdb.shape)
+
+df= pd.concat([ylp, amz, imdb], ignore_index=True, axis=0)
+print(df.shape)
+
+
+nlp = spacy.load("en_core_web_sm")
+stopwords = nlp.Defaults.stop_words
+punctuations = string.punctuation
+
+def preprocessing(sentences):
+  text = nlp(sentences)
+  tokens = []
+  for words in text:
+    if words.lemma_ != '-PRON-':
+      temp = words.lemma_.lower().strip()
+    else:
+      temp = words.lower_
+    tokens.append(temp)
+  cleaned_tokens = []
+  for word in tokens:
+    if word not in punctuations and word not in stopwords:
+      cleaned_tokens.append(word)
+  return cleaned_tokens
+
+X = df['reviews']
+y = df['label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+print(X_train.shape,y_test.shape)
+
+tfidf  = TfidfVectorizer(tokenizer = preprocessing)
+svm = LinearSVC()
+steps = [('tfidf',tfidf),('svm',svm)]
+pipe = Pipeline(steps)
+pipe.fit(X_train,y_train)
+y_pred = pipe.predict(X_test)
+print(classification_report(y_test,y_pred))
+print("\n\n")
+print(confusion_matrix(y_test,y_pred))
+
+st.title("Welcome!!!")
+st.write("Check whether your statement is positive/negative")
+input = st.text_input("Enter your mood")
+senti = pipe.predict([input])
+
+button = st.button("Check Mood")
+if button:
+    if senti == 1:
+        st.write("Yay! You are happy and positive")
+    if senti == 0:
+        st.write("Sorry! It is sad to see you sad")
